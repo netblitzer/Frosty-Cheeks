cfcb902c3428ccc0c71bf3a23b26e088ff2c66b9
Luke Miller <thenetblitzer@gmail.com>
2015-04-02 20:28:26 -0400

    Frame updating and initial stuff for the FrameEditor.

---
+++ FrostyCheeksEditor/FrostyCheeksEditor/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FrostyCheeksEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BinaryWriter output = new BinaryWriter(File.OpenWrite("frame1.dat"));
+            output.Write("bg.png");
+            output.Write(20);
+            output.Write(20);
+        }
+    }
+}
